b5ff33d0977917b4c105f5dd737bc1921282f059
Oskar van Velden <oskar@rakso.nl>
2019-06-24 14:46:27 +0200

    Add country code mapping

---
+++ europe-countries.js
@@ -0,0 +1,34 @@
+/**
+ * Simple mapping of country codes (ISO 3166 alpha-3) with their Dutch full names
+ */
+
+var countryCodeMapping = {
+  BEL: 'België',
+  BGR: 'Bulgarije',
+  CYP: 'Cyprus',
+  DNK: 'Denemarken',
+  DEU: 'Duitsland',
+  EST: 'Estland',
+  FIN: 'Finland',
+  FRA: 'Frankrijk',
+  GRC: 'Griekenland',
+  HUN: 'Hongarije',
+  IRL: 'Ierland',
+  ITA: 'Italië',
+  HRV: 'Kroatië',
+  LVA: 'Letland',
+  LTU: 'Litouwen',
+  LUX: 'Luxemburg',
+  MLT: 'Malta',
+  NLD: 'Nederland',
+  AUT: 'Oostenrijk',
+  POL: 'Polen',
+  PRT: 'Portugal',
+  ROU: 'Roemenië',
+  SVK: 'Slovenië',
+  SVN: 'Slowakije',
+  ESP: 'Spanje',
+  CZE: 'Tsjechië',
+  GBR: 'Verenigd Koninkrijk',
+  SWE: 'Zweden',
+};
